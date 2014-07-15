04669706744a00e448608756dad62b213bc5d062
Stephane Chamberland <Stephane.Chamberland@ec.gc.ca>
2014-07-15 21:36:40 +0000

    xlf support

--- include/AIX-powerpc7/xlf13/Makefile.arch.mk
+++ include/AIX-powerpc7/xlf13/Makefile.arch.mk
@@ -2,5 +2,5 @@
 ## File: LOCALBUILD/include/Makefile.ec_arch.mk (AIX-powerpc7/xlf13)
 ##
 
-OPTF_MODULE  = -qmodule=$(BUILDMOD)
+OPTF_MODULE  = -qmoddir=$(BUILDMOD)
 ## ====================================================================
