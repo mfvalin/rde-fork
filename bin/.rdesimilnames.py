8a4fa003f603e35c2b708c0451718c11142f8c74
Stephane Chamberland <Stephane.Chamberland@ec.gc.ca>
2014-11-10 15:03:12 +0000

    similnames was not looking properly for name.ext when similar match was found in other dirs

--- bin/.rdesimilnames.py
+++ bin/.rdesimilnames.py
@@ -13,26 +13,35 @@ if __name__ == "__main__":
     (options,args) = parser.parse_args()
     myname = args[0]
     mydirlist = args[1:]
-    a = []
+    mymatches = []
     for mydir in mydirlist:
         filelist = []
+        mymatches0 = []
         for myfile in listfiles(mydir):
             mybasename = os.path.basename(myfile)
             (myname0, myext) = os.path.splitext(mybasename)
             if mybasename[0:4] != '.rm.' and myext not in ('.o','.mod'):
                 if os.path.basename(myname) in [myname0,myname0+'.',mybasename] or myname == myfile:
-                    a.append(mybasename)
+                    mymatches0.append(mybasename)
                 else:
                     if mybasename not in filelist:
                         filelist.append(mybasename)
-        if (len(a)<=0):
+        if (len(mymatches0)<=0):
             for myfile in glob(mydir+'/'+myname+'*'):
                 mybasename = os.path.basename(myfile)
                 (myname0, myext) = os.path.splitext(mybasename)
                 if mybasename[0:4] != '.rm.' and myext not in ('.o','.mod'):
-                    a.append(mybasename)
-        if (len(a)<=0):
-            a += difflib.get_close_matches(myname,filelist,n=5)#,n=9,cutoff=0.5
-    c = set(a) #sorted(set(a))
+                    mymatches0.append(mybasename)
+        if (len(mymatches0)<=0):
+            mymatches0 += difflib.get_close_matches(myname,filelist)#,n=9,cutoff=0.5
+        mymatches += mymatches0
+    c = set(mymatches) #sorted(set(a))
     d = [os.path.basename(item) for item in c]
-    print(string.join(d,' '))
+    e = []
+    for item in d:
+        if (myname in (os.path.splitext(item)[0],item)):
+            e.append(item)
+    if (len(e)<=0):
+        print(string.join(d,' '))
+    else:
+        print(string.join(e,' '))
