d47f48163788aabc6deac45a327ab268bc545e9d
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2014-12-17 17:04:52 +0000

    Intel: force mkl and fp-model precise

--- include/1.1.0/Linux_x86-64/intel13sp1u2/Makefile.arch.mk
+++ include/1.1.0/Linux_x86-64/intel13sp1u2/Makefile.arch.mk
@@ -2,9 +2,7 @@
 ## File: LOCALBUILD/include/Makefile.ec_arch.mk (Linux_x86-64/intel13sp1u2)
 ##
 
-LAPACK     = 
-BLAS       = 
-EC_MKL     = -mkl -fp-model precise
+#EC_MKL = -mkl
 #RDE_OPTF_MODULE = -module $(BUILDMOD)
-RDE_FFLAGS = -diag-disable 7713 -g -traceback $(EC_MKL)
+RDE_FFLAGS = -diag-disable 7713 -fp-model strict -g -traceback
 ## ====================================================================
