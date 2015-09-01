205f6bba5258b22cad8bf8c1d1c899193134e4e5
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2015-09-01 15:56:15 +0000

    check clock skew ; b11 boot script

--- etc/ssm/1.0.b7.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/ksh
-export rde_bndl=ENV/x/rde/1.0.b7 #${1}
-export rde_version=${rde_bndl##*/}
-#export rde=/users/dor/armn/sch/SsmBundles/data/rde/${rde_version}
-export rde=/users/dor/armn/env/SsmBundles/ENV/d/x/rde/${rde_version}
-. $rde/bin/.rde.env_setup.dot rde ${rde_version} all $rde $rde
