647de40dc6538557a1e02ca68798a38a85bba6ae
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2015-09-07 02:36:36 +0000

    bump version number

--- etc/ssm/1.0.b12.sh
+++ etc/ssm/1.0.b12.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-export rde_bndl=ENV/x/rde/1.0.b11 #${1}
+export rde_bndl=ENV/x/rde/1.0.b12 #${1}
 export rde_version=${rde_bndl##*/}
 #export rde=/users/dor/armn/sch/SsmBundles/data/rde/${rde_version}
 export rde=/users/dor/armn/env/SsmBundles/ENV/d/x/rde/${rde_version}
