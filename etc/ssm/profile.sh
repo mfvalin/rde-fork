307ea40c9bd2eb8d2b166949332c65c9ad4e2a60
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2015-09-21 18:04:58 +0000

    update rde install instructions; clockskew tolerence now 1s; add forchk support

--- etc/ssm/profile.sh
+++ etc/ssm/profile.sh
@@ -1,6 +1,7 @@
 #!/bin/ksh
-export rde_bndl=ENV/x/rde/1.0.b12 #${1}
+export rde_bndl=DEV_BNDL
 export rde_version=${rde_bndl##*/}
 #export rde=/users/dor/armn/sch/SsmBundles/data/rde/${rde_version}
-export rde=/users/dor/armn/env/SsmBundles/ENV/d/x/rde/${rde_version}
+#export rde=/users/dor/armn/env/SsmBundles/ENV/d/x/rde/${rde_version}
+export rde=DEV_DIR
 . $rde/bin/.rde.env_setup.dot rde ${rde_version} all $rde $rde
