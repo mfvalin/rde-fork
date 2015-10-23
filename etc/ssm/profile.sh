6e98059ec5d547236357d15a1fc44fde958643eb
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2015-10-23 20:43:18 +0000

    fix profile and install instructions

--- etc/ssm/profile.sh
+++ etc/ssm/profile.sh
@@ -3,5 +3,5 @@ export rde_bndl=DEV_BNDL
 export rde_version=${rde_bndl##*/}
 #export rde=/users/dor/armn/sch/SsmBundles/data/rde/${rde_version}
 #export rde=/users/dor/armn/env/SsmBundles/ENV/d/x/rde/${rde_version}
-export rde=DEV_DIR
+export rde=DEV_DIR/${rde_version}
 . $rde/bin/.rde.env_setup.dot rde ${rde_version} all $rde $rde
