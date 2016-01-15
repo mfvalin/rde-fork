4f090c7af7e0043600d29a6f61aac7958ddc6cfe
stephane.chamberland <stephane.chamberland@ec.gc.ca>
2016-01-15 20:39:48 +0000

    Added -diag-disable 5140; more options in default Makefile.user.mk

--- include/Makefile.user.mk
+++ include/Makefile.user.mk
@@ -4,19 +4,25 @@ $(info ## File: Makefile.user.mk)
 $(info ## )
 endif
 
-#VERBOSE = 1
-#OPTIL   = 2
-#OMP     = -openmp
-#MPI     = -mpi
-#LFLAGS = 
-#ifeq (intel13sp1u2,$(CONST_RDE_COMP_ARCH))
-#FFLAGS  = -C -g -traceback -warn all
-#else
-#FFLAGS  = -C -g -traceback
-#endif
-#CFLAGS  =
-#LIBAPPL = 
-#LIBPATH_USER = 
+## For details RDE Makefile variables see:
+## https://wiki.cmc.ec.gc.ca/wiki/RDE/1.0/Ref#Makefile_Vars.2C_Rules_and_targets
+
+# VERBOSE = 1
+# OPTIL   = 2
+# OMP     = -openmp
+# MPI     = -mpi
+# LFLAGS  =     # User's flags passed to the linker
+# ifeq (intel13sp1u2,$(CONST_RDE_COMP_ARCH))
+# FFLAGS  = -C -g -traceback #-warn all
+# CFLAGS  = -C -g -traceback -fp-model precise #-Wall
+# else
+# FFLAGS  = -C -g -traceback
+# CFLAGS  = -C -g -traceback 
+# endif
+# LIBAPPL = 
+# LIBPATH_USER = 
+# COMP_RULES_FILE = 
+# PROFIL  = -prof   # set to -prof to enable the profiler (default = "")
 
 ifneq (,$(DEBUGMAKE))
 $(info ## ==== Makefile.user.mk [END] ========================================)
